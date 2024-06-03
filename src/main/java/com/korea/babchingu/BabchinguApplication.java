500acc5492d03b275ba53f392f0114591b731b5b
kyj <kimyeji546408@gmail.com>
2024-06-03 14:39:22 +0900

    project init

---
+++ src/main/java/com/korea/babchingu/BabchinguApplication.java
@@ -0,0 +1,13 @@
+package com.korea.babchingu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabchinguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabchinguApplication.class, args);
+	}
+
+}
