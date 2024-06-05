5bb914f10fa0105cc56550d167531050ac7799bc
dlfmalr <161660992+dlfmalr@users.noreply.github.com>
2024-06-05 15:27:41 +0900

    board create (#63)

--- src/main/java/com/korea/babchingu/board/BoardService.java
+++ src/main/java/com/korea/babchingu/board/BoardService.java
@@ -1,20 +1,60 @@
 package com.korea.babchingu.board;
 
+import com.korea.babchingu.DataNotFoundException;
+import com.korea.babchingu.image.Image;
+import com.korea.babchingu.image.ImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final ResourceLoader resourceLoader;
+    private final ImageRepository imageRepository;
 
-    public Board create(String title, String content) {
+    public Board create(String title, String content, List<MultipartFile> images) {
         Board board = new Board();
         board.setTitle(title);
         board.setContent(content);
 
+
         boardRepository.save(board);
+
+        for (MultipartFile image : images) {
+            Image img = new Image();
+            img.setBoard(board);
+            img.setUrl(storeImage(image)); // 또는 img.setImageData(image.getBytes());
+
+            imageRepository.save(img);
+        }
 
         return board;
     }
+
+    private String storeImage(MultipartFile file) {
+        if (!file.isEmpty())
+            try {
+                String path = resourceLoader.getResource("classpath:/static").getFile().getPath();
+                File fileFolder = new File( path + "/image");
+                if (!fileFolder.exists())
+                    fileFolder.mkdirs();
+                String filePath = "/image/" + UUID.randomUUID().toString() + "." + file.getContentType().split("/")[1];
+                file.transferTo(Paths.get(path + filePath));
+                return filePath;
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+        return null;
+    }
+
 }
