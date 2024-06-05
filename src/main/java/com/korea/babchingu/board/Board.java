67fdcaacb45e5d753cbc65568b2b57a834fef8d5
dlfmalr <sonhyeseung05@gmail.com>
2024-06-05 15:25:29 +0900

    board create

--- src/main/java/com/korea/babchingu/board/Board.java
+++ src/main/java/com/korea/babchingu/board/Board.java
@@ -1,11 +1,11 @@
 package com.korea.babchingu.board;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.korea.babchingu.image.Image;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,4 +16,7 @@ public class Board {
     private Long id;
     private String title;
     private String content;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    private List<Image> images;
 }
