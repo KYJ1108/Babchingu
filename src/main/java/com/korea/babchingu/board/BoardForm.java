8bb52c272a63cee1568c81edd3249287239ef71d
dlfmalr <sonhyeseung05@gmail.com>
2024-06-05 11:34:39 +0900

    board create(title, content)

---
+++ src/main/java/com/korea/babchingu/board/BoardForm.java
@@ -0,0 +1,17 @@
+package com.korea.babchingu.board;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardForm {
+    @NotEmpty(message = "제목을 입력해주세요")
+    private String title;
+    @NotEmpty(message = "내용을 입력해주세요")
+    @Size(max = 5000, message = "내용은 최대 5000자까지 입력할 수 있습니다.")
+    private String content;
+}
