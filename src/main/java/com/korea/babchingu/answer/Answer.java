6b77ddaae68aa99cf6e85995fc5be6f2546cdb07
dlfmalr <161660992+dlfmalr@users.noreply.github.com>
2024-06-07 17:08:40 +0900

    MyOAuth update (#80)

---
+++ src/main/java/com/korea/babchingu/answer/Answer.java
@@ -0,0 +1,26 @@
+package com.korea.babchingu.answer;
+
+import com.korea.babchingu.board.Board;
+import com.korea.babchingu.comment.Comment;
+import com.korea.babchingu.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    private Comment comment;
+
+    @ManyToOne
+    private Member member;
+}
