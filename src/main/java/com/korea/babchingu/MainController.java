64c0cb96864667239c629f36f30938170a9fc533
dlfmalr <161660992+dlfmalr@users.noreply.github.com>
2024-06-07 10:39:25 +0900

    search title, restName (#68)

--- src/main/java/com/korea/babchingu/MainController.java
+++ src/main/java/com/korea/babchingu/MainController.java
@@ -1,12 +1,21 @@
 package com.korea.babchingu;
 
+import com.korea.babchingu.board.Board;
+import com.korea.babchingu.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
+
+    private final MainService mainService;
 
     @GetMapping("/")
     public String main() {
@@ -16,5 +25,14 @@ public class MainController {
     @GetMapping("/map")
     public String map() {
         return "map";
+    }
+
+    @PostMapping("/search")
+    public String search(Model model,@RequestParam String searchInput) {
+
+        List<Board> searchBoard = mainService.getSearchList(searchInput);
+        model.addAttribute("searchBoard", searchBoard);
+
+        return "search";
     }
 }
