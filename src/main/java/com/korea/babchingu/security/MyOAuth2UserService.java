8805546ecd65a2d434ed2f71938b21a1950a6785
useruserriely <psy1004.syp@gmail.com>
2024-06-07 14:39:09 +0900

    social login solve

--- src/main/java/com/korea/babchingu/security/MyOAuth2UserService.java
+++ src/main/java/com/korea/babchingu/security/MyOAuth2UserService.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,26 +24,71 @@ public class MyOAuth2UserService extends DefaultOAuth2UserService {
 
         OAuth2User user = super.loadUser(userRequest);
 
-        String sub = user.getAttribute("sub");
-        String pass = "";
-        String name = user.getAttribute("name");
-//        String email = user.getAttribute("email");
+        // 계정 -> sub
+        // 비밀번호 -> ""
+        // 닉네임 -> name
+        // email -> email
 
-        Member member = memberRepository.findByLoginId(sub).orElse(null);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        MySocialUser mySocialUser;
+
+        switch (registrationId) {
+            case "google" -> mySocialUser = googleService(user);
+            case "kakao" -> mySocialUser = kakaoService(user);
+            case "naver" -> mySocialUser = naverService(user);
+            default -> throw new IllegalStateException("Unexpected value: " + registrationId);
+        }
+
+        Member member = memberRepository.findByLoginId(mySocialUser.getSub()).orElse(null);
 
         if(member == null) {
 
             member = new Member();
-            member.setLoginId(sub);
-            member.setPassword(pass);
-            member.setNickname(name);
-//            member.setEmail(email);
-//            member.setCreateDate(LocalDateTime.now());
+            member.setLoginId(mySocialUser.getSub());
+            member.setPassword(mySocialUser.getPass());
+            member.setNickname(mySocialUser.getName());
+            member.setEmail(mySocialUser.getEmail());
+            member.setCreateDate(LocalDateTime.now());
 
             memberRepository.save(member);
         }
 
+
         return super.loadUser(userRequest);
+    }
+
+    public MySocialUser googleService(OAuth2User user) {
+        String sub = user.getAttribute("sub");
+        String pass = "";
+        String name = user.getAttribute("name");
+        String email = user.getAttribute("email");
+
+        return new MySocialUser(sub, pass, name, email);
+    }
+
+    public MySocialUser kakaoService(OAuth2User user) {
+        String sub = user.getAttribute("id").toString();
+        String pass = "";
+
+        Map<String, Object> kakaoAccount = user.getAttribute("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>)kakaoAccount.get("profile");
+        String name = (String)profile.get("nickname");
+
+        String email = (String)kakaoAccount.get("email");
+
+        return new MySocialUser(sub, pass, name, email);
+    }
+
+    public MySocialUser naverService(OAuth2User user) {
+        Map<String, Object> response = user.getAttribute("response");
+        String sub = response.get("id").toString();
+        String pass = "";
+        String name = response.get("name").toString();
+        String email = response.get("email").toString();
+
+        return new MySocialUser(sub, pass, name, email);
     }
 }
 
