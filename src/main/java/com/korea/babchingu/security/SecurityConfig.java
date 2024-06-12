a10ba60291e06e84016e285d5d6c8d993f41aba0
KYJ1108 <108640336+KYJ1108@users.noreply.github.com>
2024-06-12 15:43:40 +0900

    search Post (#98)

--- src/main/java/com/korea/babchingu/security/SecurityConfig.java
+++ src/main/java/com/korea/babchingu/security/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/signup", "/login", "/findId").permitAll()
+                        .requestMatchers("/login","/signup", "/findPw").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
