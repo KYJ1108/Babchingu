7d6715a71a9e42d5fd7f3df91f0ed32bb86033a2
dlfmalr <161660992+dlfmalr@users.noreply.github.com>
2024-06-20 17:42:40 +0900

    회원 검색 기능 (#130)
    * board detail
    
    * board detail update
    
    * board detail
    
    * comment answer nickname profile
    
    * board detail(profile redirect)
    
    * board image paging
    
    * member nickname search

--- src/main/java/com/korea/babchingu/MainService.java
+++ src/main/java/com/korea/babchingu/MainService.java
@@ -2,6 +2,8 @@ package com.korea.babchingu;
 
 import com.korea.babchingu.board.Board;
 import com.korea.babchingu.board.BoardService;
+import com.korea.babchingu.member.Member;
+import com.korea.babchingu.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainService {
     private final BoardService boardService;
+    private final MemberService memberService;
 //    private final TagService tagService;
 
-    public List<Board> getSearchList(String keyword) {
+    public List<Board> getBoardSearchList(String keyword) {
         return boardService.getSearchList(keyword);
+    }
+
+    public List<Member> getMemberSearchList(String keyword) {
+        return memberService.getSearchList(keyword);
     }
 
     public List<Board> getPopularBoards() {
