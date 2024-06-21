0967afcf88081acb90a0015e773d38fcf6c602ba
kyj <kimyeji546408@gmail.com>
2024-06-21 17:46:41 +0900

    button color

--- src/main/java/com/korea/babchingu/answer/AnswerController.java
+++ src/main/java/com/korea/babchingu/answer/AnswerController.java
@@ -1,9 +1,8 @@
 package com.korea.babchingu.answer;
 
-import com.korea.babchingu.DataNotFoundException;
 import com.korea.babchingu.board.Board;
+import com.korea.babchingu.board.BoardService;
 import com.korea.babchingu.comment.Comment;
-import com.korea.babchingu.comment.CommentForm;
 import com.korea.babchingu.comment.CommentService;
 import com.korea.babchingu.member.Member;
 import com.korea.babchingu.member.MemberService;
@@ -18,9 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
-import java.util.Optional;
-
-import static com.korea.babchingu.answer.QAnswer.answer;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,12 +25,19 @@ public class AnswerController {
     private final AnswerService answerService;
     private final MemberService memberService;
     private final CommentService commentService;
+    private final BoardService boardService;
     @PostMapping("/create/{id}")
     public String create(Model model, @PathVariable("id") Long id, @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Member member = memberService.getMember(principal.getName());
         Comment comment = commentService.getComment(id);
+        Board board = boardService.getBoard(id);
+
         if (bindingResult.hasErrors()) {
-            return "board_detail";
+            return "redirect:/board/%d".formatted(board.getId());
+        }
+        if (answerForm.getContent().trim().isEmpty()) {
+            // 대댓글 내용이 비어 있는 경우 처리
+            return "redirect:/board/%d".formatted(board.getId());
         }
         model.addAttribute("comment", comment);
         Answer answer = this.answerService.create(comment, answerForm.getContent(), member);
